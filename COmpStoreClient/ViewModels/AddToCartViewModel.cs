130e5444bb6789157f741e59c74f6b5bf352b56a
nguyenlamtra <nguyenlamtra95@gmail.com>
2017-11-15 14:52:44 +0700

    first commit

---
+++ COmpStoreClient/ViewModels/AddToCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using COmpStoreClient.Validation;
+using COmpStoreClient.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace COmpStoreClient.ViewModels
+{
+    public class AddToCartViewModel : CartViewModelBase
+    {
+        [MustNotBeGreaterThan(nameof(UnitsInStock)), MustBeGreaterThanZero]
+        public int Quantity { get; set; }
+    }
+}
